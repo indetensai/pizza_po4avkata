0533bd197f20f4c3d8f08ab05dd339c37442e822
indetensai <artyomvagin12@gmail.com>
2023-02-09 21:32:17 +0200

    login user_service feature + session

--- user_service/main.go
+++ user_service/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
 	"fmt"
 	"log"
+	"math/big"
 	"net"
 	"os"
 	"pizza/service"
@@ -11,6 +13,7 @@ import (
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -27,13 +30,33 @@ type server struct {
 	service.UnimplementedUserServiceServer
 }
 
+type User struct {
+	ID       primitive.ObjectID `bson:"_id"`
+	Username string             `bson:"username"`
+	Password string             `bson:"password"`
+}
+
+func GenerateRandomString(n int) (string, error) {
+	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-."
+	ret := make([]byte, n)
+	for i := 0; i < n; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			return "generating failed", err
+		}
+		ret[i] = letters[num.Int64()]
+	}
+
+	return string(ret), nil
+}
+
 func (s *server) Register(
 	ctx context.Context,
 	request *service.RegisterRequest,
 ) (*service.RegisterResponse, error) {
 	username := request.GetUsername()
-	password := []byte(request.GetPassword())
-	password, err := bcrypt.GenerateFromPassword(password, 10)
+	password_raw := []byte(request.GetPassword())
+	password, err := bcrypt.GenerateFromPassword(password_raw, 10)
 	if err != nil {
 		return nil, err
 	}
@@ -47,6 +70,40 @@ func (s *server) Register(
 	}
 	return &service.RegisterResponse{}, nil
 
+}
+
+func (s *server) Login(
+	ctx context.Context,
+	request *service.LoginRequest,
+) (*service.LoginResponse, error) {
+	username := request.GetUsername()
+	password := request.GetPassword()
+	var result User
+	err := db.Database("user_service").Collection("users").FindOne(
+		ctx,
+		bson.D{{Key: "username", Value: username}},
+		nil,
+	).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(result.Password), []byte(password))
+	if err != nil {
+		return nil, status.Error(codes.Unauthenticated, "invalid username or password")
+	}
+	session_id, err := GenerateRandomString(64)
+	if err != nil {
+		return nil, err
+	}
+	_, err = db.Database("user_service").Collection("sessions").InsertOne(
+		ctx,
+		bson.D{{Key: "username", Value: username}, {Key: "session_id", Value: session_id}},
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &service.LoginResponse{Session: session_id}, nil
 }
 
 func baza() {
@@ -69,6 +126,17 @@ func baza() {
 		context.Background(),
 		mongo.IndexModel{
 			Keys:    bson.D{{Key: "username", Value: 1}},
+			Options: options.Index().SetUnique(true),
+		},
+		nil,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Database("user_service").Collection("sessions").Indexes().CreateOne(
+		context.Background(),
+		mongo.IndexModel{
+			Keys:    bson.D{{Key: "session_id", Value: 1}},
 			Options: options.Index().SetUnique(true),
 		},
 		nil,
